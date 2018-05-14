ef313ee6f6af7e805297064a3a0ab9269edb5b5f
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2018-05-14 16:09:47 -0300

    API implementada sem banco de dados

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const caquiCoders = require('./caqui-coders');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+app.listen(port, (req,res) => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+app.get('/participante', (req, res) => {
+    res.send(caquiCoders);
+});
+
+app.get('/participante/:id', (req, res) => {
+    const participante = caquiCoders.filter(part => {
+        return part.id.toString() === req.params.id;
+    })
+    if(!participante.length) {
+        res.status(204);
+        res.send();
+        return;
+    }
+    res.send(participante[0]);
+});
+
+app.post('/participante', (req, res) => {
+    const body = req.body || {};
+    console.log(req.body);
+    if(!body.name) {
+        res.status(412);
+        res.send('Nome não encontrado');
+        return;
+    }
+    const novoParticipante = {
+        id: caquiCoders[caquiCoders.length -1].id++,
+        name: body.name,
+        funcao: body.funcao
+    };
+    caquiCoders.push(novoParticipante);
+    res.status(201);
+    res.send('Criado com sucesso!');
+})
---
+++ caqui-coders.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Claudiney Junior",
+        funcao: "Desenvolvedor",
+    },
+    {
+        id: 2,
+        name: "Alexandre Malavasi",
+        funcao: "Tech Lead",
+    },
+    {
+        id: 3,
+        name: "Lucas Santos",
+        funcao: "Desenvolvedor",
+    },
+    {
+        id: 4,
+        name: "Morganna Giovanelli",
+        funcao: "Desenvolvedor",
+    },
+    {
+        id: 5,
+        name: "Douglas Romão",
+        funcao: "Desenvolvedor & Microsoft MVP",
+    },
+]
